e003da148ce6877e6e889051460d3efbb71e0029
Valdis Saulespurens <valdis.saulespurens@gmail.com>
2018-11-20 12:19:33 +0200

    add static file hosting

--- main.js
+++ main.js
@@ -6,7 +6,9 @@ console.log("Starting Main in:"+  __dirname);
 
 
 // app.listen(process.env.PORT);
-app.listen(3011);
+app.listen(3012);
 
 app.get("/", (req, res) => res.send('Hello Express'));
 app.get("/hello", (req, res) => res.sendFile(__dirname + '/views/index.html'));
+
+app.use("/public", express.static(__dirname + "/public"));
