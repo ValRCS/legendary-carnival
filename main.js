b7cce86f9657ac805bdee75019a27126d4b4022d
Valdis Saulespurens <valdis.saulespurens@gmail.com>
2018-11-20 11:37:57 +0200

    Add static html hosting

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log("Starting Main");
+let express = require('express');
+let app = express();
+
+console.log("Starting Main in:"+  __dirname);
+
+
+
+// app.listen(process.env.PORT);
+app.listen(3011);
+
+app.get("/", (req, res) => res.send('Hello Express'));
+app.get("/hello", (req, res) => res.sendFile(__dirname + '/views/index.html'));
