0e422930998552bb6bd553b73e9d079aa4819826
Valdis Saulespurens <valdis.saulespurens@gmail.com>
2018-11-20 13:34:08 +0200

    Add Json

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 let express = require('express');
 let app = express();
+const mydata = [
+    {
+        name: "Fide",
+        likes: "people"
+    },
+    {
+        name: "Pussycat",
+        likes: "mice"
+    }
+]
 
 console.log("Starting Main in:"+  __dirname);
 
@@ -12,3 +22,6 @@ app.get("/", (req, res) => res.send('Hello Express'));
 app.get("/hello", (req, res) => res.sendFile(__dirname + '/views/index.html'));
 
 app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/json", (req, res) => res.json(mydata));
+
